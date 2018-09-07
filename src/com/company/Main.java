ca880e11938692241905713bba667a36f33d1daf
amrut-prabhu <amrut08@gmail.com>
2018-09-07 12:51:12 +0800

    Add documentation and minor refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,22 @@ package com.company;
 import java.io.File;
 import java.util.*;
 
-
+/**
+ * Master control class that creates all the objects and invokes all the methods needed to get the KWIC output.
+ */
 public class Main {
+
+    public static final int FIRST_INDEX = 0;
 
     private List<Line> inputLines;
     private Set<String> wordsToIgnore;
-    private File inputFile;
     private List<CircularShift> circularShifts;
-//    private List<List<Integer>> circularShiftIndices;
-//    private List<String> outputLines;
+    private File inputFile;
 
     public Main() {
         inputLines = new ArrayList<>();
         wordsToIgnore = new HashSet<>();
         circularShifts = new ArrayList<>();
-//        circularShiftIndices = new ArrayList<>();
-//        outputLines = new ArrayList<>();
     }
 
     public static void main(String[] args) throws Exception {
@@ -31,15 +31,17 @@ public class Main {
     }
 
     /**
-     * Reads data lines from input medium and stores them for processing by other modules.
+     * Reads data lines from input text file and stores them for processing by other modules.
      */
     public void input(String[] args) throws Exception {
         if (args.length == 0) {
-            throw new Exception("No file specified.\n" + "Usage: java com/company/Main <filename> <words to ignore>...");
+            throw new Exception("No input file specified.\n"
+                    + "Usage: java com/company/Main <filename> [word to ignore]...");
         }
 
-        inputFile = new File(args[0]);
+        inputFile = new File(args[FIRST_INDEX]);
 
+        // Remaining args are the keywords that should be ignored.
         setWordsToIgnore(args);
 
         Scanner scanner = new Scanner(inputFile);
@@ -50,64 +52,59 @@ public class Main {
     }
 
     private void setWordsToIgnore(String[] args) {
-        if (args.length > 1) {
-            for (int i = 1; i < args.length; i++) {
+        int numberOfArguments = args.length;
+        if (numberOfArguments > 1) {
+            for (int i = FIRST_INDEX + 1; i < numberOfArguments; i++) {
                 wordsToIgnore.add(args[i].toLowerCase());
             }
         }
     }
 
     public void circularShift() {
-        for (Line line: inputLines) {
-            for (int wordIndex = 0; wordIndex < line.getNumberOfWords(); wordIndex++) {
+        inputLines.forEach(line -> {
+            int numberOfWords = line.getNumberOfWords();
+
+            for (int wordIndex = 0; wordIndex < numberOfWords; wordIndex++) {
                 String startWord = line.getWord(wordIndex);
                 startWord = strip(startWord);
                 if (!wordsToIgnore.contains(startWord.toLowerCase())) {
                     circularShifts.add(new CircularShift(line, wordIndex));
                 }
             }
-        }
+        });
     }
 
     /**
      * Remove first and/or last characters, if they are not letters or digits.
-     * @param word
-     * @return
+     * @param word String from which the characters are to be removed.
+     * @return String which only consists of letters or digits.
      */
     private String strip(String word) {
-        char firstChar = word.charAt(0);
+        char firstChar = word.charAt(FIRST_INDEX);
         char lastChar = word.charAt(word.length() - 1);
 
         if (!Character.isDigit(firstChar) && !Character.isAlphabetic(firstChar)) {
-            word = word.substring(1);
+            word = word.substring(FIRST_INDEX + 1);
         }
 
-        if (word.length() >= 2 && !Character.isDigit(lastChar) && !Character.isAlphabetic(lastChar)) {
-            word = word.substring(0, word.length() - 2);
+        if (word.length() > 0 && !Character.isDigit(lastChar) && !Character.isAlphabetic(lastChar)) {
+            word = word.substring(FIRST_INDEX, word.length() - 1);
         }
 
         return word;
     }
 
     /**
-     * Sorter.
+     * Sorts all the circular shifts in lexicographical order.
      */
     public void alphabetize() {
         Collections.sort(circularShifts);
     }
 
     /**
-     * Outputs a listing of all circular shifts of all lines in alphabetical order.
+     * Outputs the list of all valid circular shifts of all lines in lexicographical order.
      */
     public void output() {
-//        for (int lineIndex = 0; lineIndex < inputLines.size(); lineIndex++) {
-//            String line = inputLines.get(lineIndex);
-//            for (int startIndex: circularShiftIndices.get(lineIndex)){
-//                System.out.println(line.substring(startIndex) + " " + line.substring(0, startIndex));
-//            }
-//        }
-        for (CircularShift circularShift: circularShifts) {
-            System.out.println(circularShift);
-        }
+        circularShifts.forEach(System.out::println);
     }
 }
